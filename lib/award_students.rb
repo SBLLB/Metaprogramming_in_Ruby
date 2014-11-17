9ef94aa08897e94105c4d465824a3320755d1b36
RachelNolan <mail@rachelnolan.com>
2014-11-17 15:13:22 +0000

    Used meta programming to create an attr_accessor

--- lib/award_students.rb
+++ lib/award_students.rb
@@ -6,12 +6,12 @@ class Student
 
 	def award(badge)
 		@badges << badge
-		self.class.send(:define_method, "has_#{badge}?"){return true}
 	end
 
 	def method_missing(method_name)
- 		if method_name =~  /^has_.+?\?/
-			return false
+ 		if method_name =~  /^has_(.+?)\?/
+			self.class.send(:define_method, method_name) {@badges.include?($1.to_sym) ? true : false}
+			self.send(method_name)
 		else
 			super
 		end	
